2bd04c579c9fc99b75e081e01b58bd1031cb6083
donhesse <donhesse@umich.edu>
2021-11-29 19:12:04 -0500

    moved happening into home page

--- home_page.js
+++ home_page.js
@@ -1,66 +1,141 @@
 let generatedDates = [];
 
+var app = new Vue({
+	el: '#app',
+	data: {
+		displayedEvents: [],
+		startDate: new Date(),
+		endDate: new Date(),
+		types: [7, 15, 11, 6, 10]
+	},
+	methods: {
+		validActivity(activity) {
+			if (activity.image_url.length === 0) return false;
+			if (activity.event_title.toLowerCase().includes("cancelled")) return false;
+			return true;
+		},
+		handleApiCall(response) {
+			console.log("api call response:", response.data)
+			// this.genres = [...new Set(this.originalSongs.map((song) => song["primaryGenreName"]))]
+			this.displayedEvents = response.data.filter(this.validActivity).map((activity) => {
+				return {
+					"Name": activity.event_title,
+					"About": activity.description.split("\r")[0],
+					"Picture Links": activity.image_url,
+					"Address": activity.location_name,
+					"Price": this.getPrice(activity.cost),
+					"Season": "Any",
+					"Inside": false
+				}
+			})
+		},
+		getPrice(cost) {
+			if (cost.toLowerCase().includes("free") || cost === "" || typeof cost == 'undefined') {
+				return 0
+			}
+			else return 1
+		},
+		setDates() {
+			this.startDate = new Date()
+			this.endDate.setDate(this.startDate.getDate() + 10)
+		},
+		urlDate(jsDate) {
+			console.log(jsDate)
+			return `${jsDate.getFullYear()}-${jsDate.getMonth()}-${jsDate.getDate()}`
+		},
+		urlTypes() {
+			return this.types.toString()
+		},
+		urlRange() {
+			return `${this.urlDate(this.startDate)}to${this.urlDate(this.endDate)}`
+		},
+		url() {
+			return `https://events.umich.edu/list/json?filter=types:${this.urlTypes()},&range=${this.urlRange()}&v=2&origin=*`
+		},
+		search(event) {
+			this.setDates()
+			axios({
+				method: 'get',
+				url: this.url(),
+				withCredentials: false,
+			}).then((response) => {
+				this.handleApiCall(response)
+			}).then(() => {
+				generatedDates.push.apply(generatedDates, this.displayedEvents);
+			})
+		}
+	},
+	created: function () {
+		this.search();
+	}
+})
+
 $(document).ready(function () {
-    //sidebar animations
-    $("#sidebar").mCustomScrollbar({
-        theme: "minimal"
-    });
+	console.log("GENERATED DATES UPDATED", generatedDates);
+	//sidebar animations
+	$("#sidebar").mCustomScrollbar({
+		theme: "minimal"
+	});
 
-    $('#dismiss, .overlay').on('click', function () {
-        $('#sidebar').removeClass('active');
-        $('.overlay').removeClass('active');
-    });
+	$('#dismiss, .overlay').on('click', function () {
+		$('#sidebar').removeClass('active');
+		$('.overlay').removeClass('active');
+	});
 
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').addClass('active');
-        $('.overlay').addClass('active');
-        $('.collapse.in').toggleClass('in');
-        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-    });
+	$('#sidebarCollapse').on('click', function () {
+		$('#sidebar').addClass('active');
+		$('.overlay').addClass('active');
+		$('.collapse.in').toggleClass('in');
+		$('a[aria-expanded=true]').attr('aria-expanded', 'false');
+	});
 
-    //filter selections: price and season
-    $('.price').on('click', function () {
-        $('#priceInputValue').html($(this).html());
-    });
+	//filter selections: price and season
+	$('.price').on('click', function () {
+		$('#priceInputValue').html($(this).html());
+	});
 
-    $('.season').on('click', function(){
-        $('#seasonInputValue').html($(this).html());
-    });
+	$('.season').on('click', function () {
+		$('#seasonInputValue').html($(this).html());
+	});
 
-    //find button
-    $('#findButton').on('click', function(){
-        console.log(data);
-        let price = $('#priceInputValue').html();
-        let season = $('#seasonInputValue').html();
-        switch (price){
-            case "free":
-                price = 0;
-                break;
-            case "$":
-                price = 1;
-                break;
-            case "$$$":
-                price = 2;
-                break;
-            default: 
-                break;
-        }
-        console.log(price);
-        console.log(season); 
-        console.log(data[0]["Season"]);
-        console.log((data[0]["Season"].toLowerCase()).includes(season));
-        generatedDates = data.filter((dateDict)=>{
-            let seasonCheck =(dateDict["Season"].toLowerCase()).includes(season);
-            let priceCheck = (dateDict["Price"]) === (price);
-            return seasonCheck && priceCheck;
-        });
-        console.log("filtered before: ", generatedDates);
+	//pull happening data
 
-        // Set dates in sessionStorage
-        sessionStorage.setItem('dates', JSON.stringify(generatedDates));
 
-        // Go to swipe page
-        window.location.href='swipe.html';
-    });
+	//find button
+	$('#findButton').on('click', function () {
+		console.log(data);
+		let price = $('#priceInputValue').html();
+		let season = $('#seasonInputValue').html();
+		switch (price) {
+			case "free":
+				price = 0;
+				break;
+			case "$":
+				price = 1;
+				break;
+			case "$$$":
+				price = 2;
+				break;
+			default:
+				break;
+		}
+		console.log(price);
+		console.log(season);
+		console.log(data[0]["Season"]);
+		console.log((data[0]["Season"].toLowerCase()).includes(season));
+		console.log("GENERATED DATES", generatedDates)
+		generatedDates.push.apply(generatedDates, data.filter((dateDict) => {
+			let seasonCheck = (dateDict["Season"].toLowerCase()).includes(season);
+			let priceCheck = (dateDict["Price"]) === (price);
+			return seasonCheck && priceCheck;
+		}));
+		console.log("GENERATED DATES", generatedDates)
+		console.log("filtered before: ", generatedDates);
+
+		// Set dates in sessionStorage
+		sessionStorage.setItem('dates', JSON.stringify(generatedDates));
+
+		// Go to swipe page
+		window.location.href = 'swipe.html';
+	});
 });
-
